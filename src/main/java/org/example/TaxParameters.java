1fc32d867cb7e6c99792e60718919a8a9c148f8e
Daniil Starov, Mikk Sarv <jazzzydan@hotmail.com,  mikksrv@gmail.com>
2024-10-25 16:44:29 +0300

    [edit] tests working ready to do calculator for 2025 ;)

--- src/main/java/org/example/TaxParameters.java
+++ src/main/java/org/example/TaxParameters.java
@@ -6,7 +6,7 @@ import java.math.RoundingMode;
 public class TaxParameters {
 
     static final BigDecimal PENSION_RATE = new BigDecimal("0.020");
-    static final BigDecimal EMPLOYEE_UNEMPLOYMENT_RATE = new BigDecimal ("0.016");
+    static final BigDecimal EMPLOYEE_UNEMPLOYMENT_RATE = new BigDecimal("0.016");
     static final BigDecimal INCOME_TAX_RATE = new BigDecimal("0.200");
     static final BigDecimal SOCIAL_TAX_RATE = new BigDecimal("0.330");
     static final BigDecimal EMPLOYER_UNEMPLOYMENT_TAX_RATE = new BigDecimal("0.008");
--- src/test/java/org/example/SalaryTest.java
+++ src/test/java/org/example/SalaryTest.java
@@ -14,40 +14,46 @@ public class SalaryTest {
 
     @Test
     public void calculateTaxFreeIncomeTest() {
-        GrossSalary grossSalary = new GrossSalary(BigDecimal.valueOf(1100), true, true, true);
-        assertEquals(bd("654.00"), new GrossSalary(bd("1100"), true, true, true).calculateTaxFreeIncome());
-        assertEquals(bd("654.00"), new GrossSalary(bd("1200"), true, true, true).calculateTaxFreeIncome());
-        assertEquals(bd("508.67"), new GrossSalary(bd("1400"), true, true, true).calculateTaxFreeIncome());
-        assertEquals(bd("0.00"), new GrossSalary(bd("2100"), true, true, true).calculateTaxFreeIncome());
-        assertEquals(bd("0.00"), new GrossSalary(bd("2400"), true, true, true).calculateTaxFreeIncome());
+        GrossSalary grossSalary = new GrossSalary(BigDecimal.valueOf(1100), new InputConditions(true, true, true));
+        InputConditions conditions = new InputConditions(true, true, true);
+        assertEquals(bd("654.00"), new GrossSalary(bd("1100"), conditions).calculateTaxFreeIncome());
+        assertEquals(bd("654.00"), new GrossSalary(bd("1200"), conditions).calculateTaxFreeIncome());
+        assertEquals(bd("508.67"), new GrossSalary(bd("1400"), conditions).calculateTaxFreeIncome());
+        assertEquals(bd("0.00"), new GrossSalary(bd("2100"), conditions).calculateTaxFreeIncome());
+        assertEquals(bd("0.00"), new GrossSalary(bd("2400"), conditions).calculateTaxFreeIncome());
     }
 
     @Test
     public void totalSalaryCalculationTest() {
-        assertEquals(bd("1338.00"), new GrossSalary(bd("1000"), true, true, true).totalSalaryCalculation());
+        InputConditions conditions = new InputConditions(true, true, true);
+        assertEquals(bd("1338.00"), new GrossSalary(bd("1000"), conditions).totalSalaryCalculation());
     }
 
     @Test
     public void totalDeductionsTest() {
-        GrossSalary grossSalary = new GrossSalary(bd("1400"), true, true, true);
+        InputConditions conditions = new InputConditions(true, true, true);
+        GrossSalary grossSalary = new GrossSalary(bd("1400"), conditions);
         assertEquals(bd("1349.60"), grossSalary.calculateAmountBeforeIncomeTax());
     }
 
     @Test
     public void netSalaryCalculationTest() {
-        GrossSalary grossSalary = new GrossSalary(bd("1000"), true, true, true);
+        InputConditions conditions = new InputConditions(true, true, true);
+        GrossSalary grossSalary = new GrossSalary(bd("1000"), conditions);
         assertEquals(bd("902.00"), grossSalary.netSalaryCalculation());
     }
 
     @Test
     public void negativeTaxableIncomeTest() {
-        GrossSalary grossSalary = new GrossSalary(bd("670"), true, true, true);
+        InputConditions conditions = new InputConditions(true, true, true);
+        GrossSalary grossSalary = new GrossSalary(bd("670"), conditions);
         assertEquals(bd("645.88"), grossSalary.netSalaryCalculation());
     }
 
     @Test
     public void calculateTaxFreeIncomeFromNetTest() {
-        NetSalary netSalary = new NetSalary(bd("1500.00"), true, true, true);
+        InputConditions conditions = new InputConditions(true, true, true);
+        NetSalary netSalary = new NetSalary(bd("1500.00"), conditions);
         assertEquals(bd("138.77"), netSalary.calculateTaxFreeIncome(bd("1500")));
         assertEquals(bd("0.00"), netSalary.calculateTaxFreeIncome(bd("1619.52")));
         assertEquals(bd("654.00"), netSalary.calculateTaxFreeIncome(bd("1056.24")));
@@ -57,7 +63,8 @@ public class SalaryTest {
 
     @Test
     public void calculateGrossSalaryTest() {
-        NetSalary netSalary = new NetSalary(bd("1500.00"), true, true, true);
+        InputConditions conditions = new InputConditions(true, true, true);
+        NetSalary netSalary = new NetSalary(bd("1500.00"), conditions);
         assertEquals(bd("1909.03"), netSalary.calculateGrossSalary(bd("1500.00")));
         assertEquals(bd("2100.00"), netSalary.calculateGrossSalary(bd("1619.52")));
         assertEquals(bd("1200.00"), netSalary.calculateGrossSalary(bd("1056.24")));
@@ -67,19 +74,22 @@ public class SalaryTest {
 
     @Test
     public void calculateGrossFromTotal() {
-        TotalSalary totalSalary = new TotalSalary(bd("2007"), true, true, true);
+        InputConditions conditions = new InputConditions(true, true, true);
+        TotalSalary totalSalary = new TotalSalary(bd("2007"), conditions);
         assertEquals(bd("1500.00"), totalSalary.calculateGrossSalary(bd("2007")));
     }
 
-        @Test
+    @Test
     public void pensionAmountTest() {
-        GrossSalary grossSalary = new GrossSalary(bd("1000"), true, true, true);
+        InputConditions conditions = new InputConditions(true, true, true);
+        GrossSalary grossSalary = new GrossSalary(bd("1000"), conditions);
         assertEquals(bd("20.00"), grossSalary.pensionAmount());
     }
 
     @Test
     public void unemploymentPaymentTest() {
-        GrossSalary grossSalary = new GrossSalary(bd("1000"), true, true, true);
+        InputConditions conditions = new InputConditions(true, true, true);
+        GrossSalary grossSalary = new GrossSalary(bd("1000"), conditions);
         assertEquals(bd("16.00"), grossSalary.employeeUnemploymentPaymentAmount());
     }
 }
